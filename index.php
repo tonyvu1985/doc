29cfdb05d54ec74dc6bf0615d1ab31a3001ff72c
tony vu <tony.vu@mytraining.net>
2014-09-03 16:51:42 +1000

    doc site

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<h1>Advanced Bootstrap Tutorials with sass</h1>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bootstrap SASS</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+	<h1>Advanced Bootstrap Tutorials with sass</h1>
+</body>
+</html>
