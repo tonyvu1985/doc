6a232d47bf2113157e631b91c12fcfd424668dd8
Tony Vu <vuductrung2003@gmail.com>
2014-09-04 11:35:32 +1000

    doc site

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Bootstrap SASS</title>
-	<link rel="stylesheet" href="css/main.css">
+        <meta charset="UTF-8">
+        <title>Bootstrap SASS</title>
+        <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-
-	<h1>Advanced Bootstrap Tutorials with sass</h1>
+        <header class="site-header">
+                <div class="site-logo">
+                        <a href="#">tonyvu</a>
+                </div>
+                <nav class="site-nav">
+                        <a href="#">home</a>
+                        <a href="#">about</a>
+                        <a href="#">contact</a>
+                </nav>
+        </header>
 </body>
 </html>
