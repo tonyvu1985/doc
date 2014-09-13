0dee0a4fee6761d38ded37ec285b2c336d252751
Tony Vu <vuductrung2003@gmail.com>
2014-09-13 02:44:29 -0400

    hoc site

--- LeftNav.php
+++ LeftNav.php
@@ -1,13 +1,13 @@
 <div class="featured">
 	<section class="widget">
-		<h3>Treatments</h3>
+		<h4>Treatments</h4>
 		<ul class="treatments">
 			<li><a href="">Diet & Lifestyle</a></li>
-			<li><a href="">Cupping</a></li>
-			<li><a href="">Moxibustion</a></li>
-			<li><a href="">Tuina</a></li>
+			<li><a href="cupping.php">Cupping</a></li>
+			<li><a href="moxibustion.php">Moxibustion</a></li>
+			<li><a href="tuina.php">Tuina</a></li>
 			<li><a href="">Health Funds</a></li>
-			<li><a href="">Media Articles</a></li>
+			<li><a href="article.php">Media Articles</a></li>
 		</ul>
 	</section>
 </div>
