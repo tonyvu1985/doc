14583b293d683ff47278e0b5d371aceb8b56b500
Tony Vu <vuductrung2003@gmail.com>
2014-09-20 07:46:20 -0400

    enquire

--- enquire.php
+++ enquire.php
@@ -1,23 +1,28 @@
 <div class="leftcoldiv">
-		<h3>Enquiry Form</h3>
-		<!-- pass in variable if form is valid or invalid -->
-		<form name="frmenquire" ng-submit="submitForm(frmenquire.$valid)" novalidate>
-			<input type="text" name="username" class="form-control" ng-model="user.username" ng-minlength="3" ng-maxlength="8" placeholder="Your full name">
-			<p><span ng-show="frmenquire.username.$error.minlength" class="help-block">Name is too short.</span>
-			<span ng-show="frmenquire.username.$error.maxlength" class="help-block">Name is too long.</span></p>
-
-			<p><input type="number" class="form-control" name="phone" ng-model="phone" placeholder="Phone number"></p>
-			<p ng-show="frmenquire.phone.$invalid && !frmenquire.phone.$pristine" class="help-block">Enter a valid number.</p>
-
-			<p><input type="email" class="form-control" name="email" ng-model="email" placeholder="Your Email" required></p>
-			<p ng-show="frmenquire.email.$invalid && !frmenquire.email.$pristine" class="help-block">Enter a valid email.</p>
-
-			<p><textarea name="message" class="form-control"></textarea></p>
-			<select class="form-control" name="clinic">
-				<option value="default">-- Choose your clinic--</option>
-				<option value="Sydney">Sydney</option>
-				<option value="Fairfield">Fairfield</option>
-			</select>
-			<p><button type="submit" class="btnsubmit" ng-disable="frmenquire.$invalid">Submit</button></p>
-		</form>
+<h2>Feedback Form</h2>
+<?php
+// display form if user has not clicked submit
+if (!isset($_POST["submit"])) {
+  ?>
+  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+  <p><input type="text" placeholder="Email" name="from" class="form-control"></p>
+  <p><input type="text" name="subject" placeholder="Subject" class="form-control"></p>
+  <p><textarea placeholder="Message" name="message" class="form-control"></textarea></p>
+  <button type="submit" name="submit" class="btn btn-danger">Send</button>
+  </form>
+  <?php 
+} else {    // the user has submitted the form
+  // Check if the "from" input field is filled out
+  if (isset($_POST["from"])) {
+    $from = $_POST["from"]; // sender
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    // message lines should not exceed 70 characters (PHP rule), so wrap it
+    $message = wordwrap($message, 70);
+    // send mail
+    mail("tony.vu@mytraining.net",$subject,$message,"From: $from\n");
+    echo "Thank you for sending us feedback";
+  }
+}
+?>
 </div>
