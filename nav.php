411166f9f2436c94642da8ab93cad39d0a81399f
Tony Vu <tony.vu@mytraining.net>
2014-09-22 10:00:04 +1000

    hoc site

--- nav.php
+++ nav.php
@@ -14,7 +14,7 @@
 	      <span class="icon-bar"></span>
 	      <span class="icon-bar"></span>
 	    </button>
-	    <a class="navbar-brand" href="/">Home</a>
+	    <a class="navbar-brand" href="/hoc">Home</a>
 	  </div>        
 	  <div class="navbar-collapse collapse">
 	    <ul class="nav navbar-nav">
